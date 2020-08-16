d07f194373b4d1830409d7aadd2206fdd631372b
Tapaswini kar <kar.tapaswini1111@gmail.com>
2020-08-16 13:32:39 +0530

    next commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+/*import Cards from './Components/Cards'
+import Chart from './Components/Chart';
+import CountryPicker from './Components/CountryPicker';
+*/
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+
+import { fetchData } from './api'
+
 class App extends React.Component{
+    State ={
+        data:{},
+    }
+    async componentDidMount(){
+        const fetchData = await fetchData();
+        this.setState({data:fetchData});
+    }
     render(){
-        return (<div>
-            <h1>App</h1>
+        const{data} =this.State;
+
+        return (
+        <div className={styles.container}>
+            <Cards data={data} />
+            <CountryPicker />
+            <Chart />
         </div>
-        )
+        );
     }
 }
 
